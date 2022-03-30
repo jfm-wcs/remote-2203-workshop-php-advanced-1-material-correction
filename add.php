be119359a320600224f623393be4d60d792dae6b
Jean-François Morin <jeanfrancois.morin@wildcodeschool.com>
2022-03-30 17:04:48 +0200

    revisions  & CSS

--- add.php
+++ add.php
@@ -6,20 +6,20 @@ if ($_SERVER["REQUEST_METHOD"] === 'POST') {
     $recipe = array_map('trim', $_POST);
     $recipe = array_map('htmlentities', $recipe);
     $errors = [];
-    if (empty($recipe["title"]) || empty($recipe["description"])) {
-        $errors []= "Title and description fields are mandatory.";
-    }
-    if (mb_strlen($recipe["title"]) > 255 ) {
-        $errors []= "Title lenght must be under 255 characters.";
-    }
-
-    // if (empty($recipe["title"])) {
-    //     $errors []= "Title field is mandatory.";
+    // if (empty($recipe["title"]) || empty($recipe["description"])) {
+    //     $errors []= "Title and description fields are mandatory.";
+    // }
+    // if (mb_strlen($recipe["title"]) > 10 ) {
+    //     $errors []= "Title lenght must be under 255 characters.";
     // }
 
-    // if (empty($recipe["description"])) {
-    //     $errors []= "Description field is mandatory.";
-    // }
+    if (empty($recipe["title"]) || mb_strlen($recipe["title"]) > 10) {
+        $errors["title"] = "Title field is mandatory and length must be under 10 characters.";
+    }
+
+    if (empty($recipe["description"])) {
+        $errors["description"] = "Description field is mandatory.";
+    }
 
     if (empty($errors)) {
         saveRecipe($recipe);
--- src/views/form.php
+++ src/views/form.php
@@ -5,20 +5,41 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>New recipe</title>
+    <link rel="stylesheet" href="/assets/css/style.css">
 </head>
 <body>
     <a href="/">Home</a>
     <h1>New recipe</h1>
-    <form action="" method="post">
+    <?php if(!empty($errors)): ?>
+        <ul>
+            <?php foreach($errors as $error): ?>
+            <li><?=$error?></li>
+            <?php endforeach; ?>
+        </ul>
+    <?php endif; ?>
+    <h2>Please fill the form below</h2>
+    <p class="form-info">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime pariatur distinctio voluptas qui omnis deleniti mollitia similique, nulla dolorem incidunt tenetur non fuga rem, doloribus molestias eius? Dignissimos, assumenda nobis?</p>
+
+    <form class="form-recipe" action="" method="post">
+        
         <div>
             <label for="title">Title</label>
             <input type="text" name="title" id="title" required>
+            <?php if(isset($errors["title"])): ?>
+                <!-- <p><small><?=$errors["title"]?></small></p> -->
+            <?php endif; ?>
+            
         </div>
         <div>
             <label for="description">Description</label>
             <textarea name="description" id="description" rows="10" required></textarea>
+            <?php if(isset($errors["description"])): ?>
+                <!-- <p><small><?=$errors["description"]?></small></p> -->
+            <?php endif; ?>
         </div>
-        <button>Ajouter</button>
+        <button>Ajouter</button>        
     </form>    
+
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde quas impedit nihil aut minima. Sunt repellat facilis, rem repudiandae deleniti quam minima modi aspernatur provident necessitatibus magnam maxime nulla sed.</p>
 </body>
 </html>
